6b33233ac808e38eb43d110eb10bb9cdd147b440
Diriarte01 <113225750+Diriarte01@users.noreply.github.com>
2022-11-10 19:41:44 -0500

    Limpieza

--- Lenguajes de Programación/JavaScript/Ejercicio1.js
+++
@@ -1,6 +0,0 @@
-var array = [1,30,20,75,100,28,90];
-var max = 0;
-for (var i = 0; i < array.length; i++) {
-    max = max < array[i] ? array[i] : max ;
-}
-console.log(max);
--- Lenguajes de Programación/python/ejercicio1.py
+++
@@ -1,7 +0,0 @@
-oracion1 = 'lasfloresazules'
-oracion2 = '01234567891011121314'
-
-frag1 = oracion1[3:8]
-frag2 = oracion2[3:8]
-
-print(frag1 + ' ' + frag2)
