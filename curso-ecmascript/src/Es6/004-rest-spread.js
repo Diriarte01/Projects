c26b9eb97d8a3a091bcb4f0ee9d5c865aac3bf34
Diriarte01 <danieliriarte01@gmail.com>
2023-01-21 11:18:41 -0500

    Clases

--- curso-ecmascript/src/Es6/004-rest-spread.js
+++ curso-ecmascript/src/Es6/004-rest-spread.js
@@ -4,3 +4,9 @@ let fruits = ['apple', 'orange']
 
 let [a, b] = fruits;
 console.log(a,b);
+
+// object destructuring
+
+let user = { username: 'Daniel', age:25}
+let {username, age} = user;
+console.log(username, age);
