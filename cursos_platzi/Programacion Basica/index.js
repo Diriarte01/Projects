0cb4279c97ebaf905abdbae9cec0a7d332fd9fa4
Diriarte01 <danieliriarte01@gmail.com>
2023-01-23 14:52:45 -0500

    hola con expres

---
+++ cursos_platzi/Programacion Basica/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola');
+})
+
+app.listen(8080,()=>{
+    console.log('Servidor ya arraco');
+})
